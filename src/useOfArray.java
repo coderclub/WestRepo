6a906701ee33cd4c708961ec3ea8e83d9734ee15
Fahim1988 <fahim.pdc88@gmail.com>
2018-08-30 19:23:23 -0400

    updating homework

---
+++ src/useOfArray.java
@@ -0,0 +1,16 @@
+package HomeWork5;
+
+import java.util.ArrayList;
+
+public class useOfArray {
+    public static void main(String[] args) {
+        ArrayList<String> studentName=new ArrayList<String>();
+        studentName.add("Fahim");
+        studentName.add("Nasir");
+        studentName.add("Ahmed");
+        studentName.add("Siddiki");
+
+        System.out.println(studentName.size());
+        System.out.println(studentName);
+    }
+}

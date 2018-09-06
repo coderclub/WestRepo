4e0af77699d292d2ee899a53ed963c06e3a7015b
afia <afiafarjana560@gmail.com>
2018-09-06 00:43:45 -0400

    This code checks if two strings are anagram

---
+++ src/CheckIfAnagram.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class CheckIfAnagram {
+    public static void main(String[] args) {
+        String s1 = "ARMY";
+        String s2 = "MARY";
+        int[] s1Ascii = new int[s1.length()];
+        int[] s2Ascii = new int[s2.length()];
+
+        if(s1.length()!=s2.length()) System.out.println("Not1 anagram.");
+        else {
+            for(int i=0; i<s1.length();i++){
+                s1Ascii[i]= (int) s1.charAt(i);
+                s2Ascii[i]=(int) s2.charAt(i);
+            }
+
+            Arrays.sort(s1Ascii);
+            Arrays.sort(s2Ascii);
+
+            if(Arrays.equals(s1Ascii,s2Ascii)) System.out.println("Anagram");
+            else System.out.println("Not2 anagram");
+
+        }
+    }
+}

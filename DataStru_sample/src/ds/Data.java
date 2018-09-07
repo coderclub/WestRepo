549e4e44b5d3f4b373e16a6e34176fa870b319c6
Fahim1988 <fahim.pdc88@gmail.com>
2018-09-07 16:13:29 -0400

    Adding mongoDB

---
+++ DataStru_sample/src/ds/Data.java
@@ -0,0 +1,13 @@
+package ds;
+
+public class Data {
+    public static void main(String[] args) {
+        Data.MonDay();
+        System.out.println("..");
+       
+    }
+    
+    public static void MonDay(){
+        System.out.println("init at 1:50 pm);
+}
+
---
+++ DataStru_sample/src/ds/UseLinkedList.java
@@ -0,0 +1,31 @@
+package ds;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class UseLinkedList {
+    public static void main(String[] args) {
+        List<String> list = new LinkedList<String>();
+        list.add("NY");
+        list.add("FL");
+        list.add("AZ");
+        list.add("CA");
+        /*
+        // add an element in the 3 rd position 
+	        list.add(2, "AA");
+              // add an other list to the first one 
+	        List<String> ll =new LinkedList<String>();
+	        ll.add("BB");
+	        ll.add("CC");
+	        list.addAll(ll);
+
+        Iterator it = list.iterator();
+        while(it.hasNext()) {
+            System.out.println(it.next());
+        } */
+        for(String st:list){
+            System.out.println(st);
+        }
+    }
+}
